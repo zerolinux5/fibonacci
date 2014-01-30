8c4a4e819d466c5429eda5ac9c3bf17388299d24
Jesus Magana <maganazero5@gmail.com>
2014-01-29 18:05:01 -0800

    initial recursive sequence

---
+++ fibonacci.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int f){
+	if (f <= 1){
+		return 1;
+	} else {
+		return fibonacci(f-1) ++ fibonacci(f-2);
+	}
+}
+
+int main () {
+  int question;
+  cout << "Enter starting number ";
+  cin >> question;
+  int answer = fibonacci(question);
+  cout << answer
+  return 0;
+}
